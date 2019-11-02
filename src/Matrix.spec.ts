3551a66b42297761220005b30f33095d78bdde6b
haeguri <haegyun821@gmail.com>
2019-11-02 15:18:08 +0900

    Add initCells test

--- src/Matrix.spec.ts
+++ src/Matrix.spec.ts
@@ -2,17 +2,41 @@ import Matrix from "./Matrix";
 
 describe("Matrix", () => {
   describe("initCells", () => {
-    it("should make 5x5 matrix when width/height is 5.", () => {
-      const matrix = new Matrix({ width: 5, height: 5 });
+    it("should set width, height", () => {
+      const matrix = new Matrix({ width: 6, height: 6 });
+      expect(matrix.width).toEqual(6);
+      expect(matrix.height).toEqual(6);
+    });
+
+    it("should receive positive number", () => {
+      try {
+        new Matrix({ width: -1, height: 0 });
+      } catch (e) {
+        expect(e).toBeInstanceOf(Error);
+      }
+    });
+
+    it("should receive number as only even number", () => {
+      try {
+        new Matrix({ width: 5, height: 5 });
+      } catch (e) {
+        expect(e).toBeInstanceOf(Error);
+      }
+    });
+
+    it("should make 6x6 matrix when width/height is 6.", () => {
+      const matrix = new Matrix({ width: 6, height: 6 });
       matrix.initCells();
       expect(matrix.cells).toEqual([
         // prettier-ignore
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0]
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0]
       ]);
     });
+    // it("should ");
   });
 });
--- src/Matrix.ts
+++ src/Matrix.ts
@@ -9,13 +9,21 @@ export default class Matrix {
   public cells: number[][];
 
   constructor({ width, height }: ConstructorParams) {
+    if (width % 2 !== 0 || height % 2 !== 0) {
+      throw new Error("width, height is must be even number");
+    }
+
+    if (width < 0 || height < 0) {
+      throw new Error("width, height is must be positive number");
+    }
+
     this.width = width;
     this.height = height;
   }
 
   public initCells() {
-    this.cells = Array.from<number[]>({ length: 5 }).map(_ =>
-      Array.from<number>({ length: 5 }).fill(0)
+    this.cells = Array.from<number[]>({ length: this.height }).map(_ =>
+      Array.from<number>({ length: this.width }).fill(0)
     );
   }
 }
