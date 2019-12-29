d642289926f05860b3caf423b600fa992a1d81c9
haeguri <haegyun821@gmail.com>
2019-12-29 22:27:01 +0900

    add start method

--- src/Tetris.ts
+++ src/Tetris.ts
@@ -1,9 +1,28 @@
 import Matrix from "./Matrix";
 
+interface TetrisSetting {
+  width: number;
+  height: number;
+  initialTickPeriod: number;
+}
+
 export default class Tetris {
   readonly matrix: Matrix;
 
-  constructor(width: number, height: number) {
+  private initialTickPeriod: number;
+  private tickId?: number;
+
+  constructor({ width, height, initialTickPeriod }: TetrisSetting) {
     this.matrix = new Matrix({ width, height });
+    this.initialTickPeriod = initialTickPeriod;
+  }
+
+  get isTickStarted() {
+    return this.tickId > 0;
+  }
+
+  public start() {
+    this.tickId = (setInterval(() => {},
+    this.initialTickPeriod) as unknown) as number;
   }
 }
